ad1369f6e3309e1d03db8c88647e9ab2852959c3
fdarcy <fdarcy@mail.ru>
2024-01-21 09:49:37 +0300

    test

---
+++ src/main/java/DataGenerator.java
@@ -0,0 +1,59 @@
+
+import lombok.Value;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+
+public class DataGenerator {
+
+    private DataGenerator() {
+
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+        var cities = new String[]{" Майкоп", " Горно-Алтайск", "Уфа", "Улан-Удэ", " Махачкала", "Магас", "Нальчик",
+                "Элиста", "Черкесск", "Петрозаводск", "Сыктывкар", "Симферополь", "Йошкар-Ола", "Саранск", "Якутск",
+                "Владикавказ", "Казань", "Кызыл", "Ижевск", "Абакан", "Грозный", "Чебоксары", "Барнаул", "Чита",
+                "Петропавловск-Камчатский", "Краснодар", "Красноярск", "Пермь", "Владивосток", "Ставрополь", "Хабаровск",
+                "Благовещенск", "Архангельск", "Астрахань", "Белгород", "Брянск", "Владимир", "Волгоград", "Вологда", "Воронеж",
+                "Иваново", "Иркутск", "Калининград", "Калуга", "Кемерово", "Киров", "Кострома", "Москва", "Санкт-Петербург",
+                "Ярославль", "Челябинск", "Ульяновск", "Тюмень", "Тула", "Томск", "Нарьян-Мар", "Ханты-Мансийск",
+                "Анадырь", "Салехард"};
+        return cities[new Random().nextInt(cities.length)];
+    }
+
+    public static String generateName(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.name().lastName() + faker.name().firstName();
+    }
+
+    public static String generatePhone(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
